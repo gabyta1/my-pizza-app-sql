ac80f74629c0afdbbd6b4add6f24d5895d55b301
gabyta <tamarov27@gmail.com>
2019-12-27 00:00:58 +0200

    Update

--- src/mysql/db.js
+++ src/mysql/db.js
@@ -1,21 +1,21 @@
 const mysql = require('mysql')
 
-const db = mysql.createConnection({
-  host     : 'us-cdbr-iron-east-05.cleardb.net',
-  user     : 'b164f918dfb74e',
-  password : 'a7cdfb38',
-  database : 'heroku_ae73573e7ae6edf'
-
-});
-
-
 // const db = mysql.createConnection({
-//   host     : 'remotemysql.com',
-//   user     : 'rZWnASg0PE',
-//   password : 'sMCZIaPVOi',
-//   database : 'rZWnASg0PE'
+//   host     : 'us-cdbr-iron-east-05.cleardb.net',
+//   user     : 'b164f918dfb74e',
+//   password : 'a7cdfb38',
+//   database : 'heroku_ae73573e7ae6edf'
 
 // });
+
+
+const db = mysql.createConnection({
+  host     : 'remotemysql.com',
+  user     : 'rZWnASg0PE',
+  password : 'sMCZIaPVOi',
+  database : 'rZWnASg0PE'
+
+});
 
 db.connect((err) => {
   if(err){
