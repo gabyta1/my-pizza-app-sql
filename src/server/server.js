23dd34a045536a5078c1de4729dedb13fbff4eaa
gabyta <tamarov27@gmail.com>
2019-12-26 22:52:07 +0200

    Update

--- src/server/server.js
+++ src/server/server.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 });
 
 
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
   console.log("YES HEROku")
   res.sendFile(path.join(__dirname,'/dist/my-pizza-app/index.html'));
   //res.sendFile( path.resolve('src/index.html') )
