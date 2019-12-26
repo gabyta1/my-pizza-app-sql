a877720469f25cf1d66454900a95831e053dc389
gabyta <tamarov27@gmail.com>
2019-12-26 15:58:31 +0200

    Update

--- src/server/server.js
+++ src/server/server.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 
 
 app.get('*', function(req, res) {
-  console.log("YES HEROK")
+  console.log("YES HEROKu")
   //res.sendFile(path.join(__dirname,'/dist/my-pizza-app/index.html'));
   res.sendFile( path.resolve('src/index.html') )
 
